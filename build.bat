6a1025fbdb92102f880d5ff88bebb84399eced1a
howprice <howprice@users.noreply.github.com>
2021-01-02 19:01:16 +0000

    Fixed paths in build and run batch files

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-..\..\sjasmplus\sjasmplus.exe --fullpath --sld=bin/invaders.sld --lst=bin/invaders.lst --lstlab --sym=bin/invaders.labels --msg=war src/main.asm
+sjasmplus\sjasmplus.exe --fullpath --sld=bin/invaders.sld --lst=bin/invaders.lst --lstlab --sym=bin/invaders.labels --msg=war src/main.asm
